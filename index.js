23daad0e66e7dc4f6c6e4951a3f7d01d4b95c3f3
Skylar-Brockbank <skylar.brockbank@gmail.com>
2022-03-17 20:53:25 -0700

    update indexs

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 import test from './js/drawingFunctions.js';
+import NoiseMaker from './js/noiseFunctions.js';
+import colorizer from './js/dataVisualizer.js';
 const screen = document.getElementById('screen');
-let g = new test(screen,100,100);
-g.drawRectangle();
+screen.style.height = window.innerHeight;
+screen.style.width = window.innerWidth;
+let g = new test(screen,window.innerWidth,window.innerHeight);
+let trumpet = new NoiseMaker();
+// g.drawRectangle('green');
+// g.drawHexRow(['green','brown','red','brown','brown','brown','green','green'],0,0);
+let dataLayer = trumpet.enhance(trumpet.swapXY(trumpet.noise2D(80,60)),50,100);
+let colorGrid = colorizer(dataLayer);
+const colorGridOld = [
+  ['green','brown','brown','brown','brown','brown','green','green','green','green','green','green'],
+  ['green','green','red','brown','brown','brown','green','green','green','green','green','green'],
+  ['green','green','green','brown','brown','brown','brown','green','green','green','green','green']
+];
+g.drawHexGrid(colorGrid);
